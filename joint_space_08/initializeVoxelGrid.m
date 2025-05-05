8819beef1207b2a3ed0bf3c473c0c04d87c7a436
vv15948 <167359620+vv15948@users.noreply.github.com>
2025-05-05 08:25:54 +0200

    Update initializeVoxelGrid.m

--- joint_space_08/initializeVoxelGrid.m
+++ joint_space_08/initializeVoxelGrid.m
@@ -7,4 +7,14 @@ z_range = -0.15:voxel_size:1.55;
 
 voxel_grid = zeros(length(x_range), length(y_range), length(z_range)); % Inicializace 3D mřížky
 range = [x_range;y_range;z_range];
+
+voxel_grid(1,1,1) = 1;
+voxel_grid(1,1,end) = 1;
+voxel_grid(1,end,1) = 1;
+voxel_grid(1,end,end) = 1;
+voxel_grid(end,1,1) = 1;
+voxel_grid(end,1,end) = 1;
+voxel_grid(end,end,1) = 1;
+voxel_grid(end,end,end) = 1;
+
 end

26af2847316694599b0093b972eb80f343ea6ddd
vv15948 <167359620+vv15948@users.noreply.github.com>
2025-05-08 18:05:45 +0200

    Delete joint_space_03/plot_path.m

--- joint_space_03/plot_path.m
+++
@@ -1,8 +0,0 @@
-function  plot_path(path) 
-
-        subplot(1, 2, 1);
-            plot3(path(:,1), path(:,2), path(:,3), 'c-', 'LineWidth', 2); % Tyrkysová trajektorie
-        subplot(1, 2, 2);
-            plot3(path(:,1), path(:,2), path(:,3), 'c-', 'LineWidth', 2); % Tyrkysová trajektorie 
-
-end
